e52194517d62b7007f13aa04cbfd77aaac55a425
Alexander Lewitt <alewitt2@ibm.com>
2019-05-14 15:36:45 -0400

    Use new single downloder base controller
    dont fail, just return full response

--- src/RemoteResourceController.js
+++ src/RemoteResourceController.js
@@ -32,16 +32,7 @@ module.exports = class RemoteResourceController extends BaseDownloadController {
     opt.simple = false;
     opt.resolveWithFullResponse = true;
 
-    // TODO capture etag and last-modified and save in controller to send with future calls.
-    // Add to the request at .headers.If-Modified-Since/If-None-Match
-
-    let res = await request(opt);
-    if (res.statusCode !== 200) {
-      this.log.debug(`Download ${res.statusCode} ${opt.uri || opt.url}`);
-      return Promise.reject({ statusCode: res.statusCode, body: res.body });
-    }
-    this.log.debug(`Download ${res.statusCode} ${opt.uri || opt.url}`);
-    return res;
+    return await request(opt);
   }
 
 
